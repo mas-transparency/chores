c57a8c46f6eccee5f91ce160ac95c3bfd6dafc17
Jessica <jpan68@gatech.edu>
2019-03-06 15:35:29 -0500

    Remove ChoreBox’s color differentiation, Remove POST request priority field

--- src/components/ChoreBox.js
+++ src/components/ChoreBox.js
@@ -1,62 +1,58 @@
-import React, { Component } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import Globals from '../constants/Globals';
+import React, { Component } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import Globals from "../constants/Globals";
 
 export default class ChoreBox extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    handleClick = () => {};
+  handleClick = () => {};
 
-    // TODO: use 'props.chores' to iterate over chores
-    render() {
-        const color =
-            this.props.chorePoints >= 100
-                ? Globals.COLOR.primaryColor
-                : Globals.COLOR.secondaryColor;
-        return (
-            <View style={styles.container}>
-                <View style={styles.nameContainer}>
-                    <Text style={styles.name}>{this.props.choreName}</Text>
-                </View>
-                <View
-                    style={{
-                        ...styles.pointsContainer,
-                        backgroundColor: color
-                    }}
-                >
-                    <Text style={styles.point}>{this.props.chorePoints}</Text>
-                </View>
-            </View>
-        );
-    }
+  // TODO: use 'props.chores' to iterate over chores
+  render() {
+    const color = Globals.COLOR.secondaryColor;
+    return (
+      <View style={styles.container}>
+        <View style={styles.nameContainer}>
+          <Text style={styles.name}>{this.props.choreName}</Text>
+        </View>
+        <View
+          style={{
+            ...styles.pointsContainer,
+            backgroundColor: color
+          }}
+        >
+          <Text style={styles.point}>{this.props.chorePoints}</Text>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        minWidth: 80,
-        maxWidth: 90,
-        height: 100,
-        marginBottom: 20,
-        marginHorizontal: 10
-    },
-    nameContainer: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around'
-    },
-    pointsContainer: {
-        flex: 3,
-        alignItems: 'center',
-        justifyContent: 'space-evenly'
-    },
-    name: {
-        fontSize: Globals.FONTSIZE.medium,
-        color: Globals.COLOR.secondaryColor
-    },
-    point: {
-        fontSize: Globals.FONTSIZE.large,
-        color: Globals.COLOR.backgroundColor
-    }
+  container: {
+    width: 100,
+    height: 150,
+    marginBottom: 20,
+    marginHorizontal: 10
+  },
+  nameContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around"
+  },
+  pointsContainer: {
+    flex: 3,
+    alignItems: "center",
+    justifyContent: "space-evenly"
+  },
+  name: {
+    fontSize: Globals.FONTSIZE.medium,
+    color: Globals.COLOR.secondaryColor
+  },
+  point: {
+    fontSize: Globals.FONTSIZE.large,
+    color: Globals.COLOR.backgroundColor
+  }
 });
--- src/components/ChoresDashboard.js
+++ src/components/ChoresDashboard.js
@@ -1,103 +1,103 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-    ScrollView,
-    View,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    RefreshControl
-} from 'react-native';
+  ScrollView,
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  RefreshControl
+} from "react-native";
 
-import Global from '../constants/Globals';
-import ChoreBox from './ChoreBox';
-import Globals from '../constants/Globals';
+import Global from "../constants/Globals";
+import ChoreBox from "./ChoreBox";
+import Globals from "../constants/Globals";
 
 export default class ChoresList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            chores: [],
-            refreshing: false
-        };
-    }
-
-    componentDidMount() {
-        this._onRefresh();
-    }
-
-    _onRefresh = () => {
-        this.setState({ refreshing: true });
-
-        // TODO: should we hide this later?
-        fetch('http://3.93.95.228/chores')
-            .then(response => response.json())
-            .then(responseJson => {
-                const newChores = [];
-                for (const id in responseJson) {
-                    const chore = responseJson[id];
-                    const newChore = { ...chore, id };
-                    newChores.push(newChore)
-                }
-                // console.log(newChores)
-                this.setState({ chores: newChores });
-            })
-            .then(() => {
-                this.setState({ refreshing: false });
-            });
+  constructor(props) {
+    super(props);
+    this.state = {
+      chores: [],
+      refreshing: false
     };
+  }
 
-    _onPressChore(id) {
-        const chores = this.state.chores.filter(chore => chore.id != id);
-        this.setState({ chores });
-    }
+  componentDidMount() {
+    this._onRefresh();
+  }
 
-    renderChores = () => {
-        // console.log(this.props.chores)
-        // console.log(this.props.chores.length)
-        const chores = this.state.chores.map((chore, index) => {
-            return (
-                <TouchableOpacity
-                    key={chore.id}
-                    onPress={() => this._onPressChore(chore.id)}
-                    // underlayColor={Globals.COLOR.grey}
-                >
-                    <ChoreBox
-                        choreName={chore.name}
-                        chorePoints={chore.num_chore_points}
-                    />
-                </TouchableOpacity>
-            );
-        });
-        return <View style={styles.choreContainer}>{chores}</View>;
-    };
+  _onRefresh = () => {
+    this.setState({ refreshing: true });
 
-    // TODO: use 'props.chores' to iterate over chores
-    render() {
-        // console.log(this.props.chores)
-        return (
-            <ScrollView
-                refreshControl={
-                    <RefreshControl
-                        refreshing={this.state.refreshing}
-                        onRefresh={this._onRefresh}
-                    />
-                }
-                style={styles.container}
-            >
-                {this.renderChores()}
-            </ScrollView>
-        );
-    }
+    // TODO: should we hide this later?
+    fetch("http://3.93.95.228/chores")
+      .then(response => response.json())
+      .then(responseJson => {
+        const newChores = [];
+        for (const id in responseJson) {
+          const chore = responseJson[id];
+          const newChore = { ...chore, id };
+          newChores.push(newChore);
+        }
+        // console.log(newChores)
+        this.setState({ chores: newChores });
+      })
+      .then(() => {
+        this.setState({ refreshing: false });
+      });
+  };
+
+  _onPressChore(id) {
+    // const chores = this.state.chores.filter(chore => chore.id != id);
+    // this.setState({ chores });
+  }
+
+  renderChores = () => {
+    // console.log(this.props.chores)
+    // console.log(this.props.chores.length)
+    const chores = this.state.chores.map((chore, index) => {
+      return (
+        <TouchableOpacity
+          key={chore.id}
+          onPress={() => this._onPressChore(chore.id)}
+          // underlayColor={Globals.COLOR.grey}
+        >
+          <ChoreBox
+            choreName={chore.name}
+            chorePoints={chore.num_chore_points}
+          />
+        </TouchableOpacity>
+      );
+    });
+    return <View style={styles.choreContainer}>{chores}</View>;
+  };
+
+  // TODO: use 'props.chores' to iterate over chores
+  render() {
+    // console.log(this.props.chores)
+    return (
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this._onRefresh}
+          />
+        }
+        style={styles.container}
+      >
+        {this.renderChores()}
+      </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    },
-    choreContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        flexWrap: 'wrap'
-    }
+  container: {
+    flex: 1
+  },
+  choreContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    flexWrap: "wrap"
+  }
 });
